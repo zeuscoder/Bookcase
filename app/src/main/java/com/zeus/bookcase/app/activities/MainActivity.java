bb23ea7c2c75eb839eeb0fad6894958c194c18ac
zeus <1309170466@qq.com>
2015-10-01 21:49:26 +0800

    导入EventBus并创建用例

--- app/src/main/java/com/zeus/bookcase/app/activities/MainActivity.java
+++ app/src/main/java/com/zeus/bookcase/app/activities/MainActivity.java
@@ -1,39 +1,56 @@
 package com.zeus.bookcase.app.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zeus.bookcase.R;
+import com.zeus.bookcase.app.event.FirstActivity;
+import com.zeus.bookcase.app.event.FirstEvent;
+
+import de.greenrobot.event.EventBus;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button btn;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EventBus.getDefault().register(this);
+
+        btn = (Button) findViewById(R.id.main);
+        tv = (TextView) findViewById(R.id.tv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onEventMainThread(FirstEvent event) {
+        String msg = event.getMsg();
+        Log.d("Test",msg);
+        tv.setText(msg);
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }

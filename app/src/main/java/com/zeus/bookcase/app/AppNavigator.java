3f47033137d4c94d09336af9587bde1bb150eea5
zeus <1309170466@qq.com>
2015-10-14 22:22:57 +0800

    create lib-common and build gradle.

--- app/src/main/java/com/zeus/bookcase/app/AppNavigator.java
+++ app/src/main/java/com/zeus/bookcase/app/AppNavigator.java
@@ -1,7 +1,23 @@
 package com.zeus.bookcase.app;
 
+import android.app.Activity;
+
 /**
  * Created by zeus_coder on 2015/10/8.
  */
 public class AppNavigator implements com.zeus.skeleton.app.AppNavigator{
+    @Override
+    public void goHome(Activity activity) {
+
+    }
+
+    @Override
+    public void goUserCenter(Activity activity) {
+
+    }
+
+    @Override
+    public void openUiFromNotification(SubIntent subIntent) {
+
+    }
 }

6414a9c176fb375defe49f5b4bd37e931e18cd9c
zeus <1309170466@qq.com>
2015-10-27 23:58:18 +0800

    创建BaseActivity

---
+++ ui-user/src/main/java/com/zeus/ui_user/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.zeus.ui_user.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zeus_coder on 2015/10/27.
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

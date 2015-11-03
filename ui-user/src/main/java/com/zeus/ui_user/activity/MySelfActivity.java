7b6177e26333315f279f652bc4ed29e65f831cd5
zeus <1309170466@qq.com>
2015-11-03 22:27:40 +0800

    添加修改user_fragment雏形

--- ui-user/src/main/java/com/zeus/ui_user/activity/MySelfActivity.java
+++ ui-user/src/main/java/com/zeus/ui_user/activity/MySelfActivity.java
@@ -8,11 +8,12 @@ import com.zeus.ui_user.R;
 /**
  * Created by zeus_coder on 2015/11/1.
  */
-public class MySelfActivity extends Activity {
+public class MySelfActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user__fragment_myself);
+        initTopButton(R.string.user_myself, 0,0);
     }
 }

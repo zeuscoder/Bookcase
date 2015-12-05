afb0f64792df123d998f7055f96afca06337eece
zeus <1309170466@qq.com>
2015-12-05 22:56:41 +0800

    提交tab

---
+++ ui-user/src/main/java/com/zeus/ui_user/activity/BookCollectionTabActivity.java
@@ -0,0 +1,36 @@
+package com.zeus.ui_user.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zeus.common.tabs.MaterialTab;
+import com.zeus.common.tabs.MaterialTabListener;
+
+/**
+ * Created by zeus_coder on 2015/12/2.
+ */
+public class BookCollectionTabActivity extends Activity implements MaterialTabListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab tab) {
+
+    }
+}

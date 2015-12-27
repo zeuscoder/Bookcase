70c05e379e8437f0198b968c6aac3dad9b2fb1e3
zeus <1309170466@qq.com>
2015-12-27 22:49:54 +0800

    更新market页面

--- market/src/main/java/com/zeus/market/MarketFragment.java
+++ market/src/main/java/com/zeus/market/MarketFragment.java
@@ -11,21 +11,19 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.zeus.common.NonGridViewAdapter;
 import com.zeus.common.widget.NonScrollingGridView;
+import com.zeus.market.adapter.MarketShopListAdapter;
 
 /**
  * Created by zeus_coder on 2015/11/7.
  */
 public class MarketFragment extends Fragment {
 
-    //存放各功能图片
-    private Integer[] mFunctionPics = {R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon};
-    private NonScrollingGridView specialAdView;
-    private GridView gridView;
-    private ImageView imageView;
+    private ListView shopList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,71 +42,17 @@ public class MarketFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View EntryView = inflater.inflate(R.layout.market_fragment_main, container,
+        View EntryView = inflater.inflate(R.layout.market_fragment_index, container,
                 false);
-/*        gridView = (GridView) EntryView.findViewById(R.id.ads);
-        gridView.setAdapter(new ImageAdapter(this.getActivity()));*/
         return EntryView;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        specialAdView = (NonScrollingGridView) view.findViewById(R.id.market_ads);
-        imageView = (ImageView) view.findViewById(R.id.news);
-        /*
-        * 填充数据
-        * */
-        int[] images = new int[] {R.mipmap.ads_1,R.mipmap.ads_2,R.mipmap.ads_3,R.mipmap.ads_4};
-        specialAdView.setAdapter(new NonGridViewAdapter(getActivity(), images));
-        specialAdView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(),"he",Toast.LENGTH_SHORT).show();
-            }
-        });
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(),"he",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getActivity(), BookDiscountActivity.class));
-            }
-        });
+        shopList = (ListView) view.findViewById(R.id.market_list_shop);
+        shopList.setAdapter(new MarketShopListAdapter(getActivity()));
     }
 
- /*   public class ImageAdapter extends BaseAdapter {
 
-        private Context mContext;
-        private LayoutInflater inflater;
-
-        public ImageAdapter(Context c){
-            mContext = c;
-            inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public int getCount() {
-            return mFunctionPics.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if(convertView == null) {
-                convertView = inflater.inflate(R.layout.ids_grid_view_item, null);
-            }
-            ImageView imageView = Tools.ViewHolder.get(convertView, R.id.icon);
-            imageView.setImageResource(R.drawable.icon);
-            return convertView;
-        }
-    }*/
 }
---
+++ market/src/main/java/com/zeus/market/adapter/MarketShopListAdapter.java
@@ -0,0 +1,78 @@
+package com.zeus.market.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zeus.market.R;
+
+/**
+ * Created by zeus_coder on 2015/12/27.
+ */
+public class MarketShopListAdapter extends BaseAdapter{
+
+    private Context context;
+
+    public MarketShopListAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 7;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.market_main_item_hottest_shop, null);
+            viewHolder = new ViewHolder();
+            viewHolder.photo = (ImageView) view.findViewById(R.id.market_shop_photo);
+            viewHolder.name = (TextView) view.findViewById(R.id.market_shop_name);
+            viewHolder.time = (TextView) view.findViewById(R.id.market_shop_time);
+            viewHolder.description = (TextView) view.findViewById(R.id.market_shop_description);
+            viewHolder.item1 = (ImageView) view.findViewById(R.id.market_shop_book_item1);
+            viewHolder.item2 = (ImageView) view.findViewById(R.id.market_shop_book_item2);
+            viewHolder.item3 = (ImageView) view.findViewById(R.id.market_shop_book_item3);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.photo.setBackgroundResource(R.mipmap.ads_1);
+        viewHolder.name.setText("Zeus旗舰店");
+        viewHolder.time.setText("今天 0点45分");
+        viewHolder.description.setText("粉丝特权 最新最热的书单");
+        viewHolder.item1.setBackgroundResource(R.mipmap.ads_2);
+        viewHolder.item2.setBackgroundResource(R.mipmap.ads_3);
+        viewHolder.item3.setBackgroundResource(R.mipmap.ads_4);
+        return view;
+    }
+
+    class ViewHolder {
+        private ImageView photo;
+        private TextView name;
+        private TextView time;
+        private TextView description;
+        private ImageView item1;
+        private ImageView item2;
+        private ImageView item3;
+    }
+}

433ac1ef1dc61a29038f4ab5ba8932405bf1ed2d
zeus <1309170466@qq.com>
2016-01-09 23:50:01 +0800

    完成网络错误显示界面

--- ui-user/src/main/java/com/zeus/ui_user/fragment/FirstFragment.java
+++ ui-user/src/main/java/com/zeus/ui_user/fragment/FirstFragment.java
@@ -2,6 +2,7 @@ package com.zeus.ui_user.fragment;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
@@ -9,6 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.zeus.ui_user.R;
+import com.zeus.ui_user.view.ErrorView;
 
 /**
  * Created by zeus_coder on 2015/12/2.
@@ -17,10 +22,30 @@ public class FirstFragment  extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView text = new TextView(container.getContext());
-        text.setText("Fragment content");
-        text.setGravity(Gravity.CENTER);
+        return inflater.inflate(R.layout.user_fragment_order, container, false);
+    }
 
-        return text;
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final ErrorView mErrorView = (ErrorView) view.findViewById(R.id.error_view);
+        mErrorView.setOnRetryListener(new ErrorView.RetryListener() {
+            @Override
+            public void onRetry() {
+                Toast.makeText(getActivity(), R.string.info_retrying, Toast.LENGTH_SHORT).show();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mErrorView.setConfig(ErrorView.Config.create()
+                                .title(getString(R.string.error_title_damn))
+                                .titleColor(getResources().getColor(R.color.apptheme_primary))
+                                .subtitle(getString(R.string.error_subtitle_failed_one_more_time))
+                                .retryText(getString(R.string.error_view_retry))
+                                .build());
+                    }
+                }, 5000);
+            }
+        });
     }
 }
